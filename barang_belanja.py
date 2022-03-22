053ab75724e4089b2363872cc8de290a82cae24f
Ery D <edwiyanto@gmail.com>
2022-03-22 18:06:40 +0700

    Add files via upload
    Replace barang_belanja.py

---
+++ barang_belanja.py
@@ -0,0 +1,38 @@
+'''
+Fungsi      :   daftar_belanja 
+Parameter   :   list data belanja
+Deskripsi   :   fungsi ini digunakan untuk menampilkan
+                daftar barang yang sudah dibeli oleh
+                konsumen, dimana tercantum nilai total
+                yang harus dibayarnya.
+'''
+
+def daftar_belanja(databelanjaan):
+    header_belanjaan = '''
+    ****************************************************************
+    *                 Selamat Datang di WARUNG SANCA               *
+    ****************************************************************'''
+    header_belanjaan2 ='''    *                 DAFTAR BELANJA ANDA SAAT INI                 *
+    ================================================================
+    |  No  |   Nama Barang   | Harga        |  Qty |   Total       |
+    |==============================================================|
+    |      |                 |              |      |               |'''
+    footer_belanjaan =  '''    |==============================================================|'''
+    footer_belanjaan2 = '''    |--------------------------------------------------------------|'''
+    footer_belanjaan3 = '''    |        Terima kasih sudah berbelanja di WARUNG SANCA         |'''
+    footer_belanjaan4 = '''    |______________________________________________________________|'''
+    print(header_belanjaan)
+
+    if len(databelanjaan) > 0:
+        print(header_belanjaan2)
+        total_bayar = 0
+        for i in range(0,len(databelanjaan)):
+            harga = format(databelanjaan[i]['harga'],',d').replace(',','.')
+            total = format(databelanjaan[i]['total'],',d').replace(',','.')
+            print(f"    | {i+1:>4} | {databelanjaan[i]['item']:^15} |Rp. {harga:>8}  | {databelanjaan[i]['jumlah']:>4} |Rp. {total:>8}   |")
+            total_bayar += databelanjaan[i]['total']
+        print(footer_belanjaan)
+        print(f"    |                            Total Bayar       |Rp. {format(total_bayar,',d').replace(',','.'):>8}   |")
+        print(footer_belanjaan)
+        print(footer_belanjaan3)
+        print(footer_belanjaan4)
